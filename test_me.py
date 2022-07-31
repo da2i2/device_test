10809efe3668d806fb3875ce034d8ab88432ddc7
da2i2 <110165348+da2i2@users.noreply.github.com>
2022-07-30 22:45:19 -0900

    Update test_me.py

--- test_me.py
+++ test_me.py
@@ -116,6 +116,11 @@ print("================== Stress Test Done ====================")
 
 os.system("git add *")
 os.system('git commit -m "' + Device_name + ' added"')
+
+os.system("git config --global credential.https://github.com.username da2i2")
+os.system("git config --global credential.https://github.com.password Aiub@123456")
+
+
 os.system("git push")
 os.system("git pull")
 
